639c3bfb6ae81719455ec2131e50402c1d3029a0
igorbasko01 <igorbasko@gmail.com>
2020-02-01 19:46:40 +0200

    Finalize the sample page

--- stuff-doer/static/js/tasks.js
+++ stuff-doer/static/js/tasks.js
@@ -2,12 +2,19 @@ $(function () {
     //start the tree in an autocollapsed state
 //    $('#tasks-tree ul').hide(400);
 
+    $('#dropdownMenuButton').on('click', function (e) {
+        console.log("Button pressed...");
+    });
+
     $('#tasks-tree li').on('click', function (e) {
-        e.stopPropagation(); // prevent links from toggling the nodes
-        $(this).children('ul').slideToggle();
+        if (!e.originalEvent.target.classList.contains("btn")) {
+            e.stopPropagation(); // prevent links from toggling the nodes
+            console.log("Class: " + $(this)[0].className);
+            $(this).children('ul').slideToggle();
+        }
     });
 
     // This code opens all hyperlinks in a new window
     // and avoids anchors
-    $('#tasks-tree a').not('[href="#"]').attr('target', '_blank');
+//    $('#tasks-tree a').not('[href="#"]').attr('target', '_blank');
 });
